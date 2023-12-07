a4c73474a5e4903e4b82471399deeb87df625d48
leonmhl <leonmoehl@outlook.de>
2023-12-07 17:42:04 +0200

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Leon";
+string myLastName = "Möhl";
+//Console.WriteLine("Hello "+myName);
+Console.WriteLine($"Hello, {myName} {myLastName}!");
